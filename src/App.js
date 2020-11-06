fb2da887e758454a8175a157cffba40644de6d1d
vistritpandey <vistritpandey@gmail.com>
2020-11-06 08:47:31 +0530

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Sidebar from './Sidebar.js'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello darkness my old friend</h1>
+      <Sidebar />
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <p>I'm the Sidebar</p>
+        </div>
+    )
+}
+
+export default Sidebar
