03cd005c5eb93e462d69d8791d2d4aeb48d87338
vistritpandey <vistritpandey@gmail.com>
2020-11-07 08:50:39 +0530

    k

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Sidebar.css'
+import DonutLargeIcon from "@material-ui/icons/DonutLarge"
+import { IconButton } from '@material-ui/core'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>I'm the Sidebar</h1>
+            <div className="sidebar__header">
+                <div className="sidebar__headerRight">
+                <IconButton>
+                    <DonutLargeIcon />
+                </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
