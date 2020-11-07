aa58c931b61bc7712422d29693c723902928a3ef
vistritpandey <vistritpandey@gmail.com>
2020-11-07 10:22:02 +0530

    k

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,11 +1,10 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
-import './Sidebar.css'
+import './SidebarChat.css'
 
 function SidebarChat() {
     return (
         <div className='sidebarChat'>
-            <h1>Add New Chat</h1>
             <Avatar />
             <div className="sidebarChat__info">
                 <h2>Room Name</h2>
