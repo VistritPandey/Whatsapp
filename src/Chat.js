234e683134fcbb41bbd7f9e254905b2e8146c423
vistritpandey <vistritpandey@gmail.com>
2020-11-09 16:53:52 +0530

    m

--- src/Chat.js
+++ src/Chat.js
@@ -1,19 +1,29 @@
 import { Avatar, IconButton } from '@material-ui/core'
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
 import MoreVert from "@material-ui/icons/MoreVert"
 import AttachFile from "@material-ui/icons/AttachFile"
 import SearchOutlined from "@material-ui/icons/SearchOutlined"
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon"
 import MicIcon from "@material-ui/icons/Mic"
 import './Chat.css'
-import SelectInput from '@material-ui/core/Select/SelectInput'
 
 function Chat() {
-    const [input, setInput] = useState();
+
+    const [seed, setSeed] = useState("");
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random()* 5000));
+    }, []);
+
+    const sendMessage = (e) => {
+
+    }
+
+    const [input, setInput] = useState("");
     return (
         <div className='chat'>
             <div className="chat__header">
-                <Avatar />
+                <Avatar src={`https://avatars.dicebear.com/api/:sprites/:seed.svg`}/>
                 <div className="chat__headerInfo">
                     <h3>Room Name</h3>
                     <p>Lat seen at.....</p>
@@ -31,29 +41,23 @@ function Chat() {
                 </div>
             </div>
             <div className="chat__body">
-                <p className="chat__message">
+                <p className={`chat__message ${true && "chat__receiver"}`}>
                     <span className="chat__name">Vistrit</span>
                       message goes here
                       <span className="chat__timestamp">
                         { new Date().toUTCString()}
                       </span>
                 </p>
-                <p className="chat__message chat__receiver">
-                    <span className="chat__name">Vachi</span>
-                      message goes here
-                      <span className="chat__timestamp">
-                        { new Date().toUTCString()}
-                      </span>
-                </p>
+                
             </div>
             <div className="chat__footer">
                 <InsertEmoticonIcon />
                 <form>
-                    <input 
+                    <input value={input} onChange={e => setInput(e.target.value)}
                         placeholder="Type a message"
                         type="text"
                     />
-                    <button 
+                    <button onClick={sendMessage}
                         type="submit">
                     Send a message</button>
                 </form>
