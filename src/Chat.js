eb8f9ce10c9afd85ca9ada4184358aa5158fbb4f
vistritpandey <vistritpandey@gmail.com>
2020-11-09 08:06:21 +0530

    okay

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,29 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, IconButton } from '@material-ui/core'
 import React from 'react'
+import MoreVert from "@material-ui/icons/MoreVert"
+import AttachFile from "@material-ui/icons/AttachFile"
+import SearchOutlined from "@material-ui/icons/SearchOutlined"
 
 function Chat() {
     return (
         <div className='chat'>
             <div className="chat__header">
                 <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Lat seen at.....</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
             </div>
         </div>
     )
