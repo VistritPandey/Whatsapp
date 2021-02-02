c61f193a97b5e9ae3dce2ec0e004349d15b380b5
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-02-02 07:13:40 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC78AYPs8Zt3pQipVeVYmjr1zclzKQ8-6s",
-    authDomain: "vistrit-whatsapp.firebaseapp.com",
-    databaseURL: "https://vistrit-whatsapp.firebaseio.com",
-    projectId: "vistrit-whatsapp",
-    storageBucket: "vistrit-whatsapp.appspot.com",
-    messagingSenderId: "712361611310",
-    appId: "1:712361611310:web:c5e8bafd32d1b1c0dd8fbf",
-    measurementId: "G-356D2RRKQN"
+      // use your own config file
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
